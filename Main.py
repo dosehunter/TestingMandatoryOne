83291220555d78dda9ef214ae7cc55a6ce16938f
Arvid <Arvidanker95@gmail.com>
2020-03-29 17:10:57 +0200

    Added redirect page, and split post string

--- Main.py
+++ Main.py
@@ -4,12 +4,18 @@ class ServePage(BaseHTTPRequestHandler):
 
     def do_POST(self):
 
-        if self.path == "/checkout":
-            print("CHECKOUT!")
         try:
             content_len = int(self.headers.get('Content-Length'))
             post_body = self.rfile.read(content_len)
-            print(post_body)
+
+            catagories = post_body.decode("utf-8").split("&")
+            phones = catagories[0][catagories[0].index("=") + 1:].split("%2C")
+            phoneLines = catagories[1][catagories[1].index("=") + 1:]
+            interConnections = catagories[2][catagories[2].index("=") + 1:]
+
+            print(phones)
+            print(phoneLines)
+            print(interConnections)
         except:
             print("Noes..")
 
