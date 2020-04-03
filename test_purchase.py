3094c32370a7d5eb063162b252ddcd90e427bf7b
Arvid <Arvidanker95@gmail.com>
2020-04-03 09:38:44 +0200

    Trigger build

--- test_purchase.py
+++ test_purchase.py
@@ -153,6 +153,8 @@ class TestPurchase:
 
 
 
+# This is just an alternative
+
 ''' # Alternative 
     @pytest.mark.parametrize("purchase, input, result, expected", [
         (Purchase(10000, [], 0 ,False), True, 10200, True),        # Valid, price should increase with 200
